4b65b13b3fb97a96359a68f414ba8cb435afcb43
Jon Church <me@jonchurch.com>
2018-12-07 17:55:16 -0500

    candle example using discontinuousTimeScaleProvider for large candles

--- src/App.js
+++ src/App.js
@@ -2,24 +2,25 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { getData } from './utils'
+
+import CandleStickChart from './components/CandleStickChart'
+
 class App extends Component {
+	componentDidMount() {
+		getData().then(data => {
+			this.setState({ data })
+		})
+	}
   render() {
+	  if (this.state == null) {
+		  return <div>Loading...</div>
+	  }
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+		<CandleStickChart 
+			data={this.state.data}
+		/>
       </div>
     );
   }
---
+++ src/components/CandleStickChart.jsx
@@ -0,0 +1,101 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import { scaleTime } from "d3-scale";
+import { format } from "d3-format";
+import { timeFormat } from "d3-time-format";
+
+import { ChartCanvas, Chart } from "react-stockcharts";
+import {
+  BarSeries,
+  CandlestickSeries,
+} from "react-stockcharts/lib/series";
+import { XAxis, YAxis } from "react-stockcharts/lib/axes";
+import {
+  CrossHairCursor,
+  EdgeIndicator,
+  CurrentCoordinate,
+  MouseCoordinateX,
+  MouseCoordinateY,
+} from "react-stockcharts/lib/coordinates";
+
+import { OHLCTooltip } from "react-stockcharts/lib/tooltip";
+import { fitWidth } from "react-stockcharts/lib/helper";
+import { last } from "react-stockcharts/lib/utils";
+import { discontinuousTimeScaleProvider } from "react-stockcharts/lib/scale";
+
+
+class CandleStickChartForContinuousIntraDay extends React.Component {
+  render() {
+	  const { type, data: initialData, width, ratio } = this.props;
+	  const xScaleProvider = discontinuousTimeScaleProvider
+		  .inputDateAccessor(d => d.date)
+	  const {
+		  data,
+		  xScale,
+		  xAccessor,
+		  displayXAccessor
+	  } = xScaleProvider(initialData)
+
+    // const xAccessor = d => d.date;
+	  const start = xAccessor(last(data));
+	  console.log(start)
+	  const n = Math.max(0, data.length - 250)
+	  console.log(n)
+    const end = xAccessor(data[n]);
+    const xExtents = [start, end];
+
+    return (
+      <ChartCanvas height={400}
+          ratio={ratio}
+          width={width}
+          margin={{ left: 80, right: 80, top: 10, bottom: 30 }}
+          type={type}
+          seriesName="MSFT"
+          data={data}
+          xScale={scaleTime()}
+          xAccessor={xAccessor}
+		  displayXAccessor={displayXAccessor}
+		  xExtents={xExtents}>
+        <Chart id={1}
+            yExtents={[d => [d.high, d.low]]}
+            padding={{ top: 40, bottom: 20 }}>
+          <XAxis axisAt="bottom" orient="bottom"/>
+          <YAxis axisAt="left" orient="left" ticks={5} />
+
+          <MouseCoordinateX
+            rectWidth={60}
+            at="bottom"
+            orient="bottom"
+            displayFormat={timeFormat("%d-%m %H:%M:%S")} />
+          <MouseCoordinateY
+            at="left"
+            orient="left"
+            displayFormat={format(".2f")} />
+
+          <CandlestickSeries />
+
+          <EdgeIndicator itemType="last" orient="right" edgeAt="right"
+            yAccessor={d => d.close} fill={d => d.close > d.open ? "#6BA583" : "#FF0000"}/>
+
+          <OHLCTooltip origin={[-40, 0]} xDisplayFormat={timeFormat("%Y-%m-%d %H:%M:%S")}/>
+        </Chart>
+        <CrossHairCursor />
+      </ChartCanvas>
+    );
+  }
+}
+
+CandleStickChartForContinuousIntraDay.propTypes = {
+  data: PropTypes.array.isRequired,
+  width: PropTypes.number.isRequired,
+  ratio: PropTypes.number.isRequired,
+  type: PropTypes.oneOf(["svg", "hybrid"]).isRequired,
+};
+
+CandleStickChartForContinuousIntraDay.defaultProps = {
+  type: "svg",
+};
+CandleStickChartForContinuousIntraDay = fitWidth(CandleStickChartForContinuousIntraDay);
+
+export default CandleStickChartForContinuousIntraDay;
