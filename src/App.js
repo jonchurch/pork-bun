e2a8b21e9dfb6bb30e8ea73405046ee6aedf8e1e
Jon Church <me@jonchurch.com>
2018-12-07 22:58:41 -0500

    add newbar creation

--- src/App.js
+++ src/App.js
@@ -34,6 +34,9 @@ class App extends Component {
 			console.log('socket error:', err)
 		})
 
+		const getLastBar = () => {
+			return this.state.data.slice(0).pop()
+		}
 		socket.on('m', e => {
 			const update = parseData(e)
 			
@@ -41,13 +44,56 @@ class App extends Component {
 				console.log(`type: ${update.T || update.TYPE} ${update.PRICE || update.P}`)
 				const data = this.state.data.slice(0)
 				console.log(update)
-				data[data.length - 1].close = update.PRICE
-				this.setState({ data })
+				const lastBar = getLastBar()
+				console.log(lastBar)
+				let resolution = 1//= sub.resolution
+				// if (resolution.includes('D')) {
+				// 	// 1 day in minutes === 1440
+				// 	resolution = 1440
+				// } else if (resolution.includes('W')) {
+				// 	// 1 week in minutes === 10080
+				// 	resolution = 10080
+				// } 
+
+				var coeff = resolution * 60
+				// console.log({coeff})
+				var rounded = Math.floor(update.LASTUPDATE / coeff) * coeff
+				var lastBarSec = lastBar.date.getTime() / 1000
+				console.log({lastBarSec})
+				var _lastBar
+
+				if (rounded > lastBarSec) {
+					// create a new candle, use last close as open **PERSONAL CHOICE**
+					_lastBar = {
+						date: new Date(rounded * 1000),
+						open: lastBar.close,
+						high: lastBar.close,
+						low: lastBar.close,
+						close: update.PRICE,
+						volume: update.volume
+					}
+					data.push(_lastBar)
+					this.setState({data})
+					console.log('new bar!')
+					
+				} else {
+					// update lastBar candle!
+					if (update.price < lastBar.low) {
+						lastBar.low = update.PRICE
+					} else if (update.price > lastBar.high) {
+						lastBar.high = update.PRICE
+					}
+					lastBar.volume += update.volume
+					lastBar.close = update.PRICE
+					data[data.length - 1] = lastBar
+					this.setState({data})
+				}
+				// data[data.length - 1].close = update.PRICE
+				// this.setState({ data })
 			} else if (update && update.T === 0) {
 				console.log('Other update:',update)
 			}
 		})
-
 		function parseData(raw) {
 			const { TRADE, CURRENT } = ccHelper.STATIC.TYPE
 			const id = raw.split("~").shift()
