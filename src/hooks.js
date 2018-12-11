a823d8e775e9f29f05812985679cf22f063d5bf5
Jon Church <me@jonchurch.com>
2018-12-10 23:49:03 -0500

    implement useRealtimeData custom hook, update lastBar (no new bars yet)

--- src/hooks.js
+++ src/hooks.js
@@ -1,5 +1,78 @@
-import { useReducer, useMemo } from 'react'
+import { useReducer, useMemo, useEffect } from 'react'
 import groupBy from 'lodash.groupby'
+import io from 'socket.io-client'
+
+import ccHelper from './CCMapHelper'
+
+const socket_url = 'wss://streamer.cryptocompare.com'
+const socket = io(socket_url)
+socket.on('connect', () => {
+	console.log('=====socket connected')
+})
+socket.on('disconnect', () => {
+	console.log('=====socket disconnected')
+})
+socket.on('error', err  => {
+	console.error('=====socket error:', err)
+})
+
+function parseCCSocketData(raw) {
+	const { TRADE, CURRENT } = ccHelper.STATIC.TYPE
+	const id = raw.split("~").shift()
+	switch (id) {
+		case TRADE:
+			// let { P: price, LASTVOLUME: volume, LASTUPDATE: ts } = ccHelper.TRADE.unpack(raw)
+			const trade = ccHelper.TRADE.unpack(raw)
+			return { price: trade.P, volume: trade.Q, ts: trade.TS}
+		case CURRENT:
+			const { PRICE: price, LASTVOLUME: volume, LASTUPDATE: ts } = ccHelper.CURRENT.unpack(raw)
+			return { ts, price, volume }
+			// return null
+		default:
+			return null
+	}
+}
+export function useRealtimeData({exchange, to, from}, dispatch) {
+	useEffect(() => {
+		const base = `${exchange}~${from}~${to}`
+		const currentPriceSub = `2~${base}`
+		const tradeSub = `0~${base}`
+		const subs = [
+			// currentPriceSub,
+			tradeSub
+		]
+
+		socket.emit('SubAdd', {subs})
+
+		socket.on('m', handleSocketEvent)
+		let lastTrade = Date.now()
+		let lastPrice = Date.now()
+		function handleSocketEvent(event) {
+				const now = Date.now()
+			const update = parseCCSocketData(event)
+			// console.log({update})
+			if (update === null) return
+			// const { price } = update
+			if (update) {
+				const diff = (now - lastTrade) / 1000
+				lastTrade = now
+				console.log('trade update', update)
+				// console.log(`Trade:${p} Last:${diff}`)
+				dispatch({type: 'SOCKET_PRICE_UPDATE', payload: update, exchange, from, to})
+			}
+			// if (price) {
+			// 	const diff = (now - lastPrice) / 1000
+			// 	lastPrice = now
+			// 	console.log(`Price:${price} Last: ${diff}`)
+			// 	// dispatch({type: 'SOCKET_PRICE_UPDATE', payload: update, exchange, from, to})
+			// }
+		}
+
+		return () => {
+			socket.emit('SubRemove', {subs: [currentPriceSub, tradeSub]})
+		}
+	}, [exchange, to, from])
+}
 
 export function useCandleReducer(infoString, initialState = {}) {
 	console.log('infostring in useCandleReducer',{infoString})
@@ -11,7 +84,7 @@ export function useCandleReducer(infoString, initialState = {}) {
 	const candleReducer = function(state = initialCandleState, action) {
 				// debugger
 		const { payload } = action
-		const { candleData } = state
+		const { candleData, allTs } = state
 		// console.log('IN CANDLE REDUCER:',{state})
 		switch (action.type) {
 			case "REQUEST_CANDLES":
@@ -37,6 +110,30 @@ export function useCandleReducer(infoString, initialState = {}) {
 					allTs,
 					candleData: { ...candleData, ...newData },
 				}
+			case "SOCKET_PRICE_UPDATE":
+				console.log('update payload', {payload})
+				console.log('IN SOCKET UPDATE',{state})
+				const { price, volume } = payload
+				if (state.allTs && state.allTs.length > 0) {
+					const lastTs = state.allTs.slice(0).pop()
+					let  lastBar = {...candleData[lastTs]}
+					if (price < lastBar.low) {
+						lastBar.low = price
+					} else if (price > lastBar.high) {
+						lastBar.high = price
+					}
+					lastBar.volume += volume
+					lastBar.close = price
+					console.log('New close:',lastBar.close)
+					const newState = {
+						...state,
+						candleData: {...candleData, [lastTs]: lastBar }
+					}
+					return newState
+
+				} else {
+					return state
+				}
 			default:
 				return state
 		}
@@ -50,6 +147,19 @@ export function useCandleReducer(infoString, initialState = {}) {
 				return {
 					...state,
 					[action.id]: candleReducer(state[action.id], action)
+				}
+			case "SOCKET_PRICE_UPDATE":
+				// update all three resolutions with their own reducer?
+				console.log('websocket action',action)
+				const {exchange, from, to, payload} = action
+				const infoString = `${exchange}:${from}/${to}`
+				return {
+					...state,
+					[`${infoString}@1`]: candleReducer(state[infoString + "@1"], action),
+					[`${infoString}@60`]: candleReducer(state[infoString + "@60"], action),
+					[`${infoString}@D`]: candleReducer(state[infoString + "@D"], action),
+					// ["Coinbase:BTC/USD@60"]: candleReducer(state[], action),
+					// ["Coinbase:BTC/USD@D"]: candleReducer({}, action)
 				}
 			default:
 				return state
@@ -66,6 +176,7 @@ export function useCandleReducer(infoString, initialState = {}) {
 	console.log({infoString})
 	// const candleState = state[newInfoString] //|| initialCandleState
 	// console.log({candleState})
+	console.log('bottom of reducer',{state})
 	// const {loading, candleData, canLoadMore, allTs} = state[infoString] || initialCandleState
 	const {loading, candleData, canLoadMore, allTs} = state[newInfoString] || initialCandleState
 	return [{loading, candleData, canLoadMore, allTs}, dispatch]
