a86df5a2686b4d226daf50de5d11d144c4c3a948
Jon Church <me@jonchurch.com>
2018-12-14 01:15:31 -0500

    convert the left hand side to a ts when saving a future trend point

--- src/components/CandleStickChart.jsx
+++ src/components/CandleStickChart.jsx
@@ -26,6 +26,8 @@ import { fitWidth } from "react-stockcharts/lib/helper";
 import { discontinuousTimeScaleProvider } from "react-stockcharts/lib/scale";
 
 import withRealtimeData from '../containers/RealtimeDataWrapper'
+import { reduceResolution } from '../hooks'
+
 const OFFWHITE = "#f9f9f9"
 const RED = "#ef5350"
 const GREEN = "#48a69a"
@@ -45,20 +47,20 @@ class CandleStickChartForContinuousIntraDay extends React.Component {
 		this.xExtents = xExtents
 		this.priceFormat = props.data[0].close > 1 ? twoFixed : eightFixed
 		const trends_1 = [
-			{
-			  start: [300, 7000],
-			  end: [500, 7000],
-			  appearance: {stroke: "red", strokeWidth: 2},
-			  type: "LINE"
-		
-		},
 			// {
-			//   start: [1538524800, 7000],
-			//   end: [1543449600, 7000],
+			//   start: [300, 7000],
+			//   end: [500, 7000],
 			//   appearance: {stroke: "red", strokeWidth: 2},
 			//   type: "LINE"
 		
 		// },
+			{
+			  start: [1538524800, 7000],
+			  end: [1543449600, 7000],
+			  appearance: {stroke: "red", strokeWidth: 2},
+			  type: "LINE"
+		
+		},
 			// {
 			//   start: [1543935600, 4035.10],
 			//   end: [1544457600, 3523.25],
@@ -109,29 +111,43 @@ class CandleStickChartForContinuousIntraDay extends React.Component {
 		})
 	}
 	onDrawCompleteChart1 = trends_1 => {
-		// console.log({trends_1});
+		console.log({trends_1});
 		// we get an array of trends with their new values
-		// const newTrends = trends_1.map((trend, i) => {
-		// 	// turn these indexes to dates
-		// 	const startCandle = this.props.data[trend.start[0]]
-		// 	const endCandle = this.props.data[trend.end[0]]
-		// 	const startDate = startCandle.date.getTime() / 1000
-		// 	const endDate = endCandle.date.getTime() / 1000
-		// 	// console.log({startCandle, endCandle})
-		// 	// console.log({startDate, endDate})
-		// 	const newTrend = {
-		// 		...trend,
-		// 		start: [startDate, trend.start[1]],
-		// 		end: [endDate, trend.end[1]],
-		// 	}
-		// 	// console.log('startdate',{newTrend})
-		// 	return newTrend
+		// if its passing indexes, we need to turn them into dates
+		const newTrends = trends_1.map((trend, i) => {
+			// turn these indexes to dates
+			const startCandle = this.props.data[trend.start[0]]
+			const endCandle = this.props.data[trend.end[0]]
+			const resolution = resolution === "D" ? 86400 : 3600
+
+			console.log({resolution})
+			// we need to convert an index into a future date we can store a ts for the trend xy
+			const lastBar = this.props.data[this.props.data.length - 1]
+			const intervalsIntoFuture = trend.start[0] - this.props.data.length
+			const msToAdd = (intervalsIntoFuture * resolution) * 1000
+			const newFutureDate = new Date(lastBar.date.getTime() + msToAdd).getTime() / 1000
+			console.log({newFutureDate})
+			const startDate = startCandle ?
+				startCandle.date.getTime() / 1000 
+				: 
+				newFutureDate
+				
+			const endDate = endCandle.date.getTime() / 1000
+			console.log({startCandle, endCandle})
+			// console.log({startDate, endDate})
+			const newTrend = {
+				...trend,
+				start: [startDate, trend.start[1]],
+				end: [endDate, trend.end[1]],
+			}
+			console.log('newTrends',{newTrend})
+			return newTrend
 			 
-		// })
+		})
 		this.setState({
 			enableTrendLine: false,
-			trends_1,
-			// trends_1: newTrends
+			// trends_1,
+			trends_1: newTrends
 		});
   }
   render() {
@@ -253,8 +269,8 @@ class CandleStickChartForContinuousIntraDay extends React.Component {
 			// snapTo={d => [d.high, d.low]}
 			// onStart={() => console.log('Trendline start drag')}
 			onComplete={this.onDrawCompleteChart1}
-			trends={this.state.trends_1}
-			// trends={this.state.trends_1.map(translateTrends)}
+			// trends={this.state.trends_1}
+			trends={this.state.trends_1.map(translateTrends)}
 		/>
 
 	{/*
